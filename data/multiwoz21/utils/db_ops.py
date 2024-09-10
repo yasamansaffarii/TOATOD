9151f7fcbfb921a9fc661dfafdc87015676c41c3
Yasaman Saffari <54445561+yasamansaffarii@users.noreply.github.com>
2024-09-11 01:04:59 +0330

    Update db_ops.py

--- data/multiwoz21/utils/db_ops.py
+++ data/multiwoz21/utils/db_ops.py
@@ -1,249 +1,38 @@
-import json, random, sqlite3
-from utils.ontology import all_domains, db_domains
+import json, random
 
 class MultiWozDB(object):
     def __init__(self, db_paths):
+        """Initialize the database by loading the hospital data."""
         self.dbs = {}
-        self.sql_dbs = {}
-        for domain in all_domains:
-            with open(db_paths[domain], 'r') as f:
-                self.dbs[domain] = json.loads(f.read().lower())
+        with open(db_paths['hospital'], 'r') as f:
+            self.dbs['hospital'] = json.loads(f.read().lower())
 
-    def one_hot_vector(self, domain, num):
-        """Return number of available entities for a particular domain."""
-        vector = [0, 0, 0, 0]
-        if num == '':
-            return vector
-        if domain != 'train':
-            if num == 0:
-                vector = [1, 0, 0, 0]
-            elif num == 1:
-                vector = [0, 1, 0, 0]
-            elif num <= 3:
-                vector = [0, 0, 1, 0]
-            else:
-                vector = [0, 0, 0, 1]
-        else:
-            if num == 0:
-                vector = [1, 0, 0, 0]
-            elif num <= 5:
-                vector = [0, 1, 0, 0]
-            elif num <= 10:
-                vector = [0, 0, 1, 0]
-            else:
-                vector = [0, 0, 0, 1]
-        return vector
-
-    def add_booking_pointer(self, turn_da):
-        """Add information about the availability of the booking option."""
-        vector = [0, 0]
-        if turn_da.get('booking-nobook'):
-            vector = [1, 0]
-        if turn_da.get('booking-book') or turn_da.get('train-offerbooked'):
-            vector = [0, 1]
-        return vector
-
-    def add_db_pointer(self, domain, match_num, return_num=False):
-        """Create database pointer for all related domains."""
-        if domain in db_domains:
-            vector = self.one_hot_vector(domain, match_num)
-        else:
-            vector = [0, 0, 0, 0]
-        return vector
-
-    def add_db_indicator(self, domain, match_num, return_num=False):
-        """Create database indicator for all related domains."""
-        if domain in db_domains:
-            vector = self.one_hot_vector(domain, match_num)
-        else:
-            vector = [0, 0, 0, 0]
-
-        if vector == [0, 0, 0, 0]:
-            indicator = '[db_nores]'
-        else:
-            indicator = '[db_%s]' % vector.index(1)
-        return indicator
-
-    def get_match_num(self, constraints, return_entry=False):
-        """Create database pointer for all related domains."""
-        match = {'general': ''}
-        entry = {}
-        for domain in all_domains:
-            match[domain] = ''
-            if domain in db_domains and constraints.get(domain):
-                matched_ents = self.query_jsons(domain, constraints[domain])
-                match[domain] = len(matched_ents)
-                if return_entry:
-                    entry[domain] = matched_ents
-        if return_entry:
-            return entry
-        return match
-
-    def pointer_back(self, vector, domain):
-        """Multi-domain implementation for pointers."""
-        if domain.endswith(']'):
-            domain = domain[1:-1]
-        if domain != 'train':
-            nummap = {
-                0: '0',
-                1: '1',
-                2: '2-3',
-                3: '>3'
-            }
-        else:
-            nummap = {
-                0: '0',
-                1: '1-5',
-                2: '6-10',
-                3: '>10'
-            }
-        if vector[:4] == [0, 0, 0, 0]:
-            report = ''
-        else:
-            num = vector.index(1)
-            report = domain + ': ' + nummap[num] + '; '
-
-        if vector[-2] == 0 and vector[-1] == 1:
-            report += 'booking: ok'
-        if vector[-2] == 1 and vector[-1] == 0:
-            report += 'booking: unable'
-
-        return report
-
-    def query_jsons(self, domain, constraints, exactly_match=True, return_name=False):
-        """Returns a list of entities for a given domain based on belief state annotations."""
-        if domain == 'taxi':
-            return [{'taxi_colors': random.choice(self.dbs[domain]['taxi_colors']),
-                     'taxi_types': random.choice(self.dbs[domain]['taxi_types']),
-                     'taxi_phone': [random.randint(1, 9) for _ in range(10)]}]
-        if domain == 'police':
-            return self.dbs['police']
-        if domain == 'hospital':
-            if constraints.get('department'):
-                for entry in self.dbs['hospital']:
-                    if entry.get('department') == constraints.get('department'):
-                        return [entry]
-            else:
-                return []
-
-        valid_cons = False
-        for v in constraints.values():
-            if v not in ["not mentioned", ""]:
-                valid_cons = True
-        if not valid_cons:
-            return []
-
-        match_result = []
-
-        if 'name' in constraints:
-            for db_ent in self.dbs[domain]:
-                if 'name' in db_ent:
-                    cons = constraints['name']
-                    dbn = db_ent['name']
-                    if cons == dbn:
-                        db_ent = db_ent if not return_name else db_ent['name']
-                        match_result.append(db_ent)
-                        return match_result
-
-        for db_ent in self.dbs[domain]:
-            match = True
-            for s, v in constraints.items():
-                if s == 'name':
-                    continue
-                if s in ['people', 'stay'] or (domain == 'hotel' and s == 'day') or \
-                        (domain == 'restaurant' and s in ['day', 'time']):
-                    continue
-
-                skip_case = {"don't care": 1, "do n't care": 1, "dont care": 1, "not mentioned": 1, "dontcare": 1, "": 1}
-                if skip_case.get(v):
-                    continue
-
-                if s not in db_ent:
-                    match = False
-                    break
-
-                v = 'yes' if v == 'free' else v
-
-                if s in ['arrive', 'leave']:
-                    try:
-                        h, m = v.split(':')
-                        v = int(h) * 60 + int(m)
-                    except:
-                        match = False
-                        break
-                    time = int(db_ent[s].split(':')[0]) * 60 + int(db_ent[s].split(':')[1])
-                    if s == 'arrive' and v > time:
-                        match = False
-                    if s == 'leave' and v < time:
-                        match = False
-                else:
-                    if exactly_match and v != db_ent[s]:
-                        match = False
-                        break
-                    elif v not in db_ent[s]:
-                        match = False
-                        break
-
-            if match:
-                match_result.append(db_ent)
-
-        if not return_name:
-            return match_result
-        else:
-            if domain == 'train':
-                match_result = [e['id'] for e in match_result]
-            else:
-                match_result = [e['name'] for e in match_result]
-            return match_result
-
-    def query_sql(self, domain, constraints):
-        if not self.sql_dbs:
-            for dom in db_domains:
-                db = 'db/{}-dbase.db'.format(dom)
-                conn = sqlite3.connect(db)
-                c = conn.cursor()
-                self.sql_dbs[dom] = c
-
-        sql_query = "select * from {}".format(domain)
-        flag = True
-        for key, val in constraints.items():
-            if val == "" or val in ["dontcare", 'not mentioned', "don't care", "dont care", "do n't care"]:
-                pass
-            else:
-                if flag:
-                    sql_query += " where "
-                    val2 = val.replace("'", "''")
-                    if key == 'leaveAt':
-                        sql_query += r" " + key + " > " + r"'" + val2 + r"'"
-                    elif key == 'arriveBy':
-                        sql_query += r" " + key + " < " + r"'" + val2 + r"'"
-                    else:
-                        sql_query += r" " + key + "=" + r"'" + val2 + r"'"
-                    flag = False
-                else:
-                    val2 = val.replace("'", "''")
-                    if key == 'leaveAt':
-                        sql_query += r" and " + key + " > " + r"'" + val2 + r"'"
-                    elif key == 'arriveBy':
-                        sql_query += r" and " + key + " < " + r"'" + val2 + r"'"
-                    else:
-                        sql_query += r" and " + key + "=" + r"'" + val2 + r"'"
-
-        try:
-            print(sql_query)
-            return self.sql_dbs[domain].execute(sql_query).fetchall()
-        except:
-            return []
+    def query_jsons(self, constraints):
+        """
+        Returns a list of entities for the 'hospital' domain based on belief state annotations.
+        
+        Args:
+            constraints (dict): The constraints specified for querying the hospital database.
+        
+        Returns:
+            list: A list of matched entities satisfying the constraints.
+        """
+        # If department is specified, search for matching entries
+        if constraints.get('department'):
+            for entry in self.dbs['hospital']:
+                if entry.get('department') == constraints['department']:
+                    return [entry]
+        # If no department constraint is given, return an empty list
+        return []
 
 if __name__ == '__main__':
+    # Define the path to the hospital database
     db_paths = {
-        'attraction': 'db/attraction_db_processed.json',
-        'hospital': 'db/hospital_db_processed.json',
-        'hotel': 'db/hotel_db_processed.json',
-        'police': 'db/police_db_processed.json',
-        'restaurant': 'db/restaurant_db_processed.json',
-        'taxi': 'db/taxi_db_processed.json',
-        'train': 'db/train_db_processed.json',
+        'hospital': 'db/hospital_db_processed.json'
     }
+    # Initialize the MultiWozDB with the hospital path
     multiwoz_db = MultiWozDB(db_paths)
-    print(multiwoz_db.query_jsons('hotel', {'area': 'west'}))
+    
+    # Example query to find hospitals with a specific department
+    result = multiwoz_db.query_jsons({'department': 'radiology'})
+    print(result)
