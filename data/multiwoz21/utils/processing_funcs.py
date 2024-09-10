fd8aeddd09b529919ec00ec5ffd6103e1b667736
Yasaman Saffari <54445561+yasamansaffarii@users.noreply.github.com>
2024-09-11 01:08:17 +0330

    Update processing_funcs.py

--- data/multiwoz21/utils/processing_funcs.py
+++ data/multiwoz21/utils/processing_funcs.py
@@ -1,4 +1,6 @@
 import re
+import progressbar
+
 def restore_text(text):
     text = re.sub('=', '', text)
     text = re.sub(',', '', text)
@@ -6,53 +8,46 @@ def restore_text(text):
     return text
 
 def split_bs_text_by_domain(bs_text, bsdx_text):
+    """
+    This function is customized to handle only the 'hospital' domain.
+    """
     res_text_list = []
     bs_text = bs_text.strip('<sos_b>').strip('<eos_b>').strip()
     bsdx_text = bsdx_text.strip('<sos_b>').strip('<eos_b>').strip()
-    token_list = bsdx_text.split()
-    domain_list = []
-    for token in token_list:
-        if token.startswith('[') and token.endswith(']'):
-            domain_list.append(token)
-    if domain_list == 1: # only have one domain
-        return [bs_text], [bsdx_text]
-    else:
-        bs_list, bsdx_list = [], []
-        for idx in range(len(domain_list)):
-            curr_domain = domain_list[idx]
-            if idx == len(domain_list)-1: # last domain
-                bs_text_snippet = curr_domain + ' ' + bs_text.split(curr_domain)[1].strip()
-                bsdx_text_snippet = curr_domain + ' ' + bsdx_text.split(curr_domain)[1].strip()
-            else:
-                next_domain = domain_list[idx+1]
-                bs_text_snippet = curr_domain + ' ' + bs_text.split(curr_domain)[1].split(next_domain)[0]
-                bsdx_text_snippet = curr_domain + ' ' + bsdx_text.split(curr_domain)[1].split(next_domain)[0]
-            bs_list.append(bs_text_snippet.strip())
-            bsdx_list.append(bsdx_text_snippet.strip())
+
+    # Check if the domain is 'hospital'
+    if '[hospital]' not in bsdx_text:
+        return [], []
+
+    # Extract the portion related to 'hospital'
+    bs_list = [bs_text.split('[hospital]')[1].strip()] if '[hospital]' in bs_text else []
+    bsdx_list = [bsdx_text.split('[hospital]')[1].strip()] if '[hospital]' in bsdx_text else []
+    
     return bs_list, bsdx_list
 
 def parse_bs_bsdx(bs_text, bsdx_text):
-    # this function deals belief state from single domain
-    # we assume there is no repitive slots in the bsdx text
+    """
+    This function processes belief states within the 'hospital' domain.
+    """
     dx_token_list = bsdx_text.split()
-
     bs_name_list = bsdx_text.split()
     token_num = len(bs_name_list)
     map_dict = {}
     res_bs_text = ''
     res_bsdx_text = ''
+
     for idx in range(token_num):
         curr_slot = bs_name_list[idx]
         if curr_slot.startswith('[') and curr_slot.endswith(']'):
             continue
         else:
             if idx == token_num - 1:
-                #curr_value = bs_text.split(' ' + curr_slot + ' ')[-1].strip()
                 curr_value = bs_text.split(' ' + curr_slot + ' ')[-1].strip()
             else:
-                next_slot = bs_name_list[idx+1]
+                next_slot = bs_name_list[idx + 1]
                 curr_value = bs_text.split(' ' + curr_slot + ' ')[1].split(' ' + next_slot)[0].strip()
             map_dict[curr_slot] = curr_value
+
     for curr_slot in bs_name_list:
         if curr_slot.startswith('[') and curr_slot.endswith(']'):
             res_bs_text += curr_slot + ' '
@@ -60,17 +55,22 @@ def parse_bs_bsdx(bs_text, bsdx_text):
         else:
             res_bs_text += curr_slot + ' = ' + map_dict[curr_slot] + ' , '
             res_bsdx_text += curr_slot + ' , '
-    
+
     res_bs_text = res_bs_text.strip().strip(',').strip()
     res_bsdx_text = res_bsdx_text.strip().strip(',').strip()
     return ' '.join(res_bs_text.split()).strip(), ' '.join(res_bsdx_text.split()).strip()
 
 def overall_parsing_bs_bsdx(bs_text, bsdx_text):
+    """
+    This function processes the overall belief state texts specifically for the 'hospital' domain.
+    """
     in_bs_text, in_bsdx_text = bs_text, bsdx_text
     if bs_text == '<sos_b> <eos_b>':
         assert bsdx_text == '<sos_b> <eos_b>'
         return bs_text, bsdx_text
     bs_text_list, bsdx_text_list = split_bs_text_by_domain(bs_text, bsdx_text)
+    if not bs_text_list or not bsdx_text_list:
+        return '<sos_b> <eos_b>', '<sos_b> <eos_b>'
     res_bs_text, res_bsdx_text = ' ', ' '
     for idx in range(len(bs_text_list)):
         one_bs_text, one_bsdx_text = parse_bs_bsdx(bs_text_list[idx], bsdx_text_list[idx])
@@ -80,9 +80,12 @@ def overall_parsing_bs_bsdx(bs_text, bsdx_text):
     res_bsdx_text = '<sos_b> ' + res_bsdx_text.strip() + ' <eos_b>'
     assert restore_text(res_bs_text) == in_bs_text
     assert restore_text(res_bsdx_text) == in_bsdx_text
-    return res_bs_text,  res_bsdx_text
+    return res_bs_text, res_bsdx_text
 
 def parse_action_text(text):
+    """
+    This function parses the action text.
+    """
     in_text = text
     res_text = ''
     token_list = text.strip('<sos_a>').strip('<eos_a>').strip().split()
@@ -104,28 +107,10 @@ def parse_action_text(text):
     assert restore_text(res_text) == in_text
     return res_text
 
-def tokenized_decode(token_id_list, tokenizer, special_token_list):
-    pred_tokens = tokenizer.convert_ids_to_tokens(token_id_list)
-    res_text = ''
-    curr_list = []
-    for token in pred_tokens:
-        if token in special_token_list:
-            if len(curr_list) == 0:
-                res_text += ' ' + token + ' '
-            else:
-                curr_res = tokenizer.convert_tokens_to_string(curr_list)
-                res_text = res_text + ' ' + curr_res + ' ' + token + ' '
-                curr_list = []
-        else:
-            curr_list.append(token)
-    if len(curr_list) > 0:
-        curr_res = tokenizer.convert_tokens_to_string(curr_list)
-        res_text = res_text + ' ' + curr_res + ' '
-    res_text_list = res_text.strip().split()
-    res_text = ' '.join(res_text_list).strip()
-    return res_text
-
 def transform_id_dict(in_dict, data):
+    """
+    This function transforms an input dictionary using a tokenizer.
+    """
     res_dict = {}
     for key in in_dict:
         if key in ['pointer','turn_num','turn_domain','dial_id']:
@@ -135,6 +120,9 @@ def transform_id_dict(in_dict, data):
     return res_dict
 
 def process_dict(in_dict, data):
+    """
+    This function processes a single dictionary entry to focus on the 'hospital' domain.
+    """
     in_dict = transform_id_dict(in_dict, data)
     res_dict = in_dict.copy()
     bs_text_reform, bsdx_text_reform = overall_parsing_bs_bsdx(in_dict['bspn'], in_dict['bsdx'])
@@ -144,8 +132,10 @@ def process_dict(in_dict, data):
     res_dict['aspn_reform'] = action_text_reform
     return res_dict
 
-import progressbar
 def process_data_list(data_list):
+    """
+    This function processes a list of data entries to handle only 'hospital' domain data.
+    """
     res_list = []
     p = progressbar.ProgressBar(len(data_list))
     p.start()
@@ -154,14 +144,10 @@ def process_data_list(data_list):
         p_idx += 1
         p.update(p_idx)
         try:
-            one_res_list = [process_dict(sub_item, data) for sub_item in item]
+            one_res_list = [process_dict(sub_item, data) for sub_item in item if '[hospital]' in sub_item['bspn']]
         except: 
             continue
         res_list.append(one_res_list)
     p.finish()
-    print (len(res_list), len(data_list))
+    print(len(res_list), len(data_list))
     return res_list
-
-
-
-
